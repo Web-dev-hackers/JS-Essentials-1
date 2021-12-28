5af7fc7732fde35c6f3855b6c95e3f7aa19e0167
Essam Harous <esamfarg91@gmail.com>
2021-12-28 15:38:48 +0200

    Lesson Ready

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 console.log("Javascript is Linked with html");
-var a = "ssdf";
+
+//1 create variable for each Data type with a name of your choice: String, Number, Boolean, objects, arrays, undefined, null, float numbers
+//print all of them on the screen
+
+//2 create a variable call it 'x' and assign it to value 5 & a variable call it 'y' and assign it to value 14
+
+//3 Create a third variable called 'sum', assign the sum of x & y to it, and display it.
+
+//4 create a variable called 'sub' and assign the Subtraction of y & x to it
+
+//5 create a variable called 'multiply' and assign the Multiplication of y & x to it
+
+//6 create a variable called 'division' and assign the division of y & x to it
+
+//6 create a variable called 'divisionRemainder' and assign the module of y & x to it
