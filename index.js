60a19f00f758e389f09d94ab8f9aa7c460cd27d1
Mohammedalgzeri <m1h2a3m4m5e6d@gmail.com>
2022-01-03 19:05:55 +0200

    First one

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 console.log("index.js is Linked with html");
 
 //1 create variable for each Data type with a name of your choice: String, Number, Boolean, objects, arrays, undefined, null, float numbers
-//print all of them on the screen
+//print all of them on the screen\\
+var String= "mohammed";
+var Number= 360;
+var Boolean= true;
+var objects= {};
+var arrays= [];
+var undefined=  undefine;
+var nullvar = null;
+
 
 //2 create a variable call it 'x' and assign it to value 5 & a variable call it 'y' and assign it to value 14
+var x=5;
+var y=14;
 
 //3 Create a third variable called 'sum', assign the sum of x & y to it, and display it.
-
+var sum = x+y;
+console.log(sum);
 //4 create a variable called 'sub' and assign the Subtraction of y & x to it
+var sub = y-x;
 
 //5 create a variable called 'multiply' and assign the Multiplication of y & x to it
 
