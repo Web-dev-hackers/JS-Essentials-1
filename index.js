13f24d374ec95eaf8d99db32025b10bf0fc6456a
Bushra <btarbah2000@gmail.com>
2022-01-03 17:40:00 +0200

    Var stuff

--- index.js
+++ index.js
@@ -2,19 +2,28 @@ console.log("index.js is Linked with html");
 
 //1 create variable for each Data type with a name of your choice: String, Number, Boolean, objects, arrays, undefined, null, float numbers
 //print all of them on the screen
-
+var string ="Hello";
+var float = 0.5;
+var integers = 5;
+var boolean = true;
+var arrays =[];
+var objects = {};
+var undefined = undefined;
+var nullVar = null;
 //2 create a variable call it 'x' and assign it to value 5 & a variable call it 'y' and assign it to value 14
-
+var x = 5;
+var y = 17;
 //3 Create a third variable called 'sum', assign the sum of x & y to it, and display it.
-
+var sum = x  y ;
+console.log(sum);
 //4 create a variable called 'sub' and assign the Subtraction of y & x to it
-
+var sub = y - x ;
 //5 create a variable called 'multiply' and assign the Multiplication of y & x to it
-
+var multiply = y * x;
 //6 create a variable called 'division' and assign the division of y & x to it
-
+var division = y / x ;
 //7 create a variable called 'divisionRemainder' and assign the module of y & x to it
-
+var divisionRemainder = y % x;
 // use the comparison operators (<=, >=	, <	, >, !==	, != , ===	, ==)
 // to write these expressions in programmatic way using these variables
 var value1 = 15;
@@ -27,11 +36,14 @@ var value4 = "15";
 console.log(value1 == value4);
 
 //`value1` not equal to `value2`
-
+console.log(value1 != value2);
 //`value2` greater than `value1`
-
+console.log(value2>value1);
 //`value1` less than  `value2`
+console.log(value1<value2);
 
 //`value1` equal value and equal type  `value3`
+console.log(value1===value3);
 
 //`value1` not equal value or not equal type  `value4`
+console.log(value1!==value4);
