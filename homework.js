8f94e97a9b4ce738a69c8a92770e88014463b8cb
Bushra <btarbah2000@gmail.com>
2022-01-04 16:44:26 +0200

    Var homework

--- homework.js
+++ homework.js
@@ -2,19 +2,32 @@ console.log("homework.js is Linked with html");
 
 /** pre requirements */
 // 1 create a variable called 'firstName' give it a value of your first name
-
+var firstName = "Bushra" ;
 // 2 create a variable called 'lastName' give it a value of your last name
+var lastName = "Tarbah" ;
 
 // 3 create a variable called 'fullName' give it a value of firstName & lastName variables.
 // HINT: add space between them
-
-/** Intermediate */
+var fullName = firstName + lastName ;
+/** Intermediate */ 
 // 4 create a variable called 'counter' give it a value of 0
-
+var counter = 0 ;
 // add variable counter by one 3 times, and subtracting by one 2 times, using the Arithmetics operators eg(++, +=, --, /=, *=, -=)
 // HINT: it's value should be 1 at the end
-
+counter+=1 ;
+counter++ ;
+++counter ;
+counter-=1 ;
+--counter ;
+ 
 /** Advanced */
 
 // 5 you have 2 variables 'a' has value 3 & 'b' has value 7, I want you to assign the value of b to a & the value of a to b
 // HINT: you can create a new variable to help you out
+
+var a = 3
+var b = 7
+
+var c = b
+b = a
+a = c
