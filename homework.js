ed8f8a3d5599f8beefb030cbf7e92b00e39c6bee
taha_tarbah <tahatarbah@gmail.com>
2022-01-04 13:05:15 +0200

    Done (fire emoji)

--- homework.js
+++ homework.js
@@ -2,19 +2,40 @@ console.log("homework.js is Linked with html");
 
 /** pre requirements */
 // 1 create a variable called 'firstName' give it a value of your first name
-
+var firstName = "taha"
 // 2 create a variable called 'lastName' give it a value of your last name
-
+var lastName = "tarbah"
 // 3 create a variable called 'fullName' give it a value of firstName & lastName variables.
 // HINT: add space between them
-
+var fullName = firstName +" "+  lastName
 /** Intermediate */
 // 4 create a variable called 'counter' give it a value of 0
-
+var counter = 0
 // add variable counter by one 3 times, and subtracting by one 2 times, using the Arithmetics operators eg(++, +=, --, /=, *=, -=)
 // HINT: it's value should be 1 at the end
+
+var TNT = counter + 1
+ TNT = ++ TNT 
+ TNT = ++ TNT
+TNT = TNT-1
+TNT = --TNT
 
 /** Advanced */
 
 // 5 you have 2 variables 'a' has value 3 & 'b' has value 7, I want you to assign the value of b to a & the value of a to b
 // HINT: you can create a new variable to help you out
+var a = 3
+var b = 7
+
+var c = b
+b = a
+a = c
+
+//commit htem indevisiually
+//git add. adds everything
+//git add filename.file type to add files indevisiually
+// and commit them one by one
+
+
+/** extra task */
+ //freecode camp javascript find the lenght of strings
