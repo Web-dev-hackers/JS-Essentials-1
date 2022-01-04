4adfa33147a179fd282faadfeb65fa2e350ff0ee
Osama <elbouryosama@gmail.com>
2022-01-04 19:22:32 +0200

    DONE

--- homework.js
+++ homework.js
@@ -2,14 +2,22 @@ console.log("homework.js is Linked with html");
 
 /** pre requirements */
 // 1 create a variable called 'firstName' give it a value of your first name
-
+var firstName="Osama"
 // 2 create a variable called 'lastName' give it a value of your last name
-
+var lastname = "Elboury"
 // 3 create a variable called 'fullName' give it a value of firstName & lastName variables.
 // HINT: add space between them
+var fullName = firstName +" "+lastname;
 
 /** Intermediate */
 // 4 create a variable called 'counter' give it a value of 0
+var counter = 0
+counter++
+counter--
+counter+=2
+counter-=2
+
+
 
 // add variable counter by one 3 times, and subtracting by one 2 times, using the Arithmetics operators eg(++, +=, --, /=, *=, -=)
 // HINT: it's value should be 1 at the end
@@ -18,3 +26,9 @@ console.log("homework.js is Linked with html");
 
 // 5 you have 2 variables 'a' has value 3 & 'b' has value 7, I want you to assign the value of b to a & the value of a to b
 // HINT: you can create a new variable to help you out
+var A=3
+var B=7
+var C = A
+A = B
+B = C
+
--- index.js
+++ index.js
@@ -2,19 +2,40 @@ console.log("index.js is Linked with html");
 
 //1 create variable for each Data type with a name of your choice: String, Number, Boolean, objects, arrays, undefined, null, float numbers
 //print all of them on the screen
+var val1 = "osama";
+var val2 = 33
+var val3 = false
+var val4 = []
+var val5 = {}
+var val5 = null
+var val6 = undefined
+var val7 = 0.3
+
+
+
 
 //2 create a variable call it 'x' and assign it to value 5 & a variable call it 'y' and assign it to value 14
+var x=5
+var y=14
+
 
 //3 Create a third variable called 'sum', assign the sum of x & y to it, and display it.
-
+var sum =x+y
+console.log(sum);
 //4 create a variable called 'sub' and assign the Subtraction of y & x to it
-
+var sum =x-y
+console.log(sub);
 //5 create a variable called 'multiply' and assign the Multiplication of y & x to it
+var multiply = x*y
+console.log(multiply);
+
 
 //6 create a variable called 'division' and assign the division of y & x to it
-
+var division = x/y;
+console.log(division);
 //7 create a variable called 'divisionRemainder' and assign the module of y & x to it
-
+var divisionRemainder= x % y;
+console.log(divisionRemainder);
 // use the comparison operators (<=, >=	, <	, >, !==	, != , ===	, ==)
 // to write these expressions in programmatic way using these variables
 var value1 = 15;
@@ -27,11 +48,12 @@ var value4 = "15";
 console.log(value1 == value4);
 
 //`value1` not equal to `value2`
-
+console.log(value1!==value2);
 //`value2` greater than `value1`
-
+console.log(value2>value1);
 //`value1` less than  `value2`
-
+console.log(value1<value2);
 //`value1` equal value and equal type  `value3`
-
+console.log(value1===value3)
 //`value1` not equal value or not equal type  `value4`
+console.log(value1!==value4);
